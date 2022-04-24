7cc9ebb97f25433a7152366e6496900e95897548
OwenBratt <100020400@mvla.net>
2022-04-24 13:37:49 -0700

    Included readFromCSV call and added an endl to main

--- main.cpp
+++ main.cpp
@@ -3,11 +3,16 @@
 //
 
 #include "team_data.h"
+#include "csv_reader.h"
 
 int main() {
     using namespace NS_DATA;
+    using namespace NS_READER;
     team_data obj1;
+    vector<team_data> teamList;
     obj1.setTeamName("test");
-    std::cout << obj1.getTeamName();
+    std::cout << obj1.getTeamName() << endl;
+
+    readFromCSV("Datasets\\cbb18.csv", teamList);
     return 0;
 }
