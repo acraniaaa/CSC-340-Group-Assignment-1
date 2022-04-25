135bcd73478cfbc8d734c68299dbdd87a501291d
Javi <103536638+Javi0x0@users.noreply.github.com>
2022-04-24 20:58:14 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,15 @@ int main() {
     vector<team_data> teamList;
     obj1.setTeamName("test");
     std::cout << obj1.getTeamName() << endl;
-
+  
     readFromCSV("Datasets\\cbb18.csv", teamList);
+  //file name was the absolute path for below
+    
+    //testing getting the mean average
+    cout<<"mean defense average of "<<getMeanAveDefence(teamList)<<endl;
+  //printing the stats as in offense of a specified team - mean defense average
+  //free to set up TeamName as user defined or you can define it by giving it a specific team
+  getTeamStatRating("Villanova",teamList);
+   
     return 0;
 }
